e7a5db31669a13825d73a87307c1ff38940afce2
divit99 <41451985+divit99@users.noreply.github.com>
2022-06-15 15:08:50 +0530

    Time: 2 ms (27.23%), Space: 43.4 MB (54.17%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
@@ -16,19 +16,41 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        Queue<TreeNode>queue=new LinkedList<TreeNode>();
-        List<List<Integer>>allLevel=new LinkedList<List<Integer>>();
+//         Queue<TreeNode>queue=new LinkedList<TreeNode>();
+//         List<List<Integer>>allLevel=new LinkedList<List<Integer>>();
+//         if(root==null)return allLevel;
+//         queue.offer(root);
+//         while(!queue.isEmpty()){
+//             int n=queue.size();
+//                 List<Integer>currLevel=new LinkedList<Integer>();
+//                 for(int i=0;i<n;i++){
+//                     if(queue.peek().left!=null)queue.offer(queue.peek().left);
+//                     if(queue.peek().right!=null)queue.offer(queue.peek().right);
+//                     currLevel.add(queue.peek().val);
+//                     queue.poll();
+//                 }
+//             allLevel.add(currLevel);
+            
+//         }
+//         return allLevel;
+        
+        
+ // public List<List<Integer>> levelOrder(TreeNode root) 
+        
+        Queue<TreeNode> queue=new LinkedList<TreeNode>();
+        List<List<Integer>> allLevel=new LinkedList<List<Integer>>();
         if(root==null)return allLevel;
+        
         queue.offer(root);
         while(!queue.isEmpty()){
-            int n=queue.size();
-                List<Integer>currLevel=new LinkedList<Integer>();
-                for(int i=0;i<n;i++){
-                    if(queue.peek().left!=null)queue.offer(queue.peek().left);
-                    if(queue.peek().right!=null)queue.offer(queue.peek().right);
-                    currLevel.add(queue.peek().val);
-                    queue.poll();
-                }
+            int length=queue.size();
+            List<Integer> currLevel=new LinkedList<Integer>();
+            for(int i=0;i<length;i++){
+                if(queue.peek().left!=null)queue.offer(queue.peek().left);
+                if(queue.peek().right!=null)queue.offer(queue.peek().right);
+                currLevel.add(queue.peek().val);
+                queue.poll();
+            }
             allLevel.add(currLevel);
             
         }
