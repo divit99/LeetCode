fc29e5f3c14c0d2c37aef92e99f15939b96f1872
divit99 <41451985+divit99@users.noreply.github.com>
2022-06-29 15:30:16 +0530

    Time: 2 ms (85.32%), Space: 43.4 MB (69.00%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
@@ -28,12 +28,12 @@ class Solution {
         int sol=0;
         while(!q.isEmpty()){
             int size=q.size();
-            int max=Integer.MIN_VALUE;
-            int min=Integer.MAX_VALUE;
+            int max=q.peek().length;//Integer.MIN_VALUE;
+            int min=q.peek().length;
             for(int i=0;i<size;i++){
               answer temp=q.poll();
-                min=Math.min(min,temp.length);
-                max=Math.max(max,temp.length);
+                //min=Math.min(min,temp.length);
+                max=temp.length;
                 if(temp.root.left!=null)q.offer(new answer((temp.root.left),temp.length*2+1));
                 if(temp.root.right!=null)q.offer(new answer((temp.root.right),temp.length*2+2));
             }
