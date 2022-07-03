b4f594baa3cd2fd1eaaad9f6dc78418b1c7a9592
divit99 <41451985+divit99@users.noreply.github.com>
2022-07-03 19:11:10 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (21.19%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+//         if(n<0)return 0;
+//         if(n==0)return 1;
+        
+//         return climbStairs(n-1)+climbStairs(n-2);
+        if(n==1||n==2||n==0)return n;
+        int arr[]=new int[n+1];
+        arr[0]=0;
+        arr[1]=1;
+        arr[2]=2;
+        for(int i=3;i<=n;i++){
+            arr[i]=arr[i-1]+arr[i-2];
+        }
+        return arr[n];
+        
+        
+    }
+}
