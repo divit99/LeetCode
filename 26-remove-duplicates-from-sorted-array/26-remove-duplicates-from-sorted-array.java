95ec03366e68d8e36d128768715abca2ce454c5e
divit99 <41451985+divit99@users.noreply.github.com>
2022-07-07 21:13:08 +0530

    Time: 2 ms (42.22%), Space: 47.7 MB (55.95%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
@@ -3,22 +3,40 @@ class Solution {
         
         //edge case end 
         
-        if(nums.length==1)return 1;
+        //0 0 1 1 1 2 2 3 3 4
         
-        Stack<Integer> st=new Stack();
+        
+        
+        
+        int start=1;
+        int end=1;
+        
+        for( int i=end;end<nums.length;end++){
+            if(nums[end]!=nums[end-1]){
+                nums[start]=nums[end];
+                start++;
+            }
+        }
+        
+        return start;
+        
+        
+//         if(nums.length==1)return 1;
+        
+//         Stack<Integer> st=new Stack();
         
        
-        st.push(nums[0]);
-        for(int i=1;i<nums.length;i++){
+//         st.push(nums[0]);
+//         for(int i=1;i<nums.length;i++){
             
-            if(nums[i]!=st.peek())st.push(nums[i]);
-        }
+//             if(nums[i]!=st.peek())st.push(nums[i]);
+//         }
         
-        int ans=st.size();
-        for(int i=st.size()-1;i>=0;i--){
-            nums[i]=st.pop();
-        }
-        return ans;
+//         int ans=st.size();
+//         for(int i=st.size()-1;i>=0;i--){
+//             nums[i]=st.pop();
+//         }
+//         return ans;
           
         
     }
