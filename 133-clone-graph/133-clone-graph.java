4f7d6be8140afab56bd54a709f05b7cc46becaff
divit99 <41451985+divit99@users.noreply.github.com>
2022-07-11 12:19:18 +0530

    Time: 25 ms (99.70%), Space: 42.8 MB (78.19%) - LeetHub

--- 133-clone-graph/133-clone-graph.java
+++ 133-clone-graph/133-clone-graph.java
@@ -24,27 +24,40 @@ class Solution {
     public Node cloneGraph(Node node) {
         if(node==null)return null;
         
-        Queue<Node> queue=new LinkedList();
-        HashMap<Node,Node> visited=new HashMap();
+//         Queue<Node> queue=new LinkedList();
+//         HashMap<Node,Node> visited=new HashMap();
+//         Node curr=new Node(node.val);
+//         visited.put(node,curr);
+//         queue.offer(node);
+//         while(!queue.isEmpty()){
+//             Node newnode=queue.poll();
+//             List<Node> neighbors=visited.get(newnode).neighbors;
+            
+//             for(Node n : newnode.neighbors){
+//                 if(!visited.containsKey(n)){
+//                     visited.put(n,new Node(n.val));
+//                     queue.offer(n);
+//                 }
+//                 neighbors.add(visited.get(n));
+//             }
+//         }
+//         return curr;
+        
+        
+        Node ans=clone(node,new HashMap());
+        return ans;
+        
+    }
+    public static Node clone(Node node,HashMap<Node,Node> visited){
+        if(visited.containsKey(node))return visited.get(node);
+        //visited.put
+        
         Node curr=new Node(node.val);
         visited.put(node,curr);
-        queue.offer(node);
-        while(!queue.isEmpty()){
-            Node newnode=queue.poll();
-            List<Node> neighbors=visited.get(newnode).neighbors;
-            
-            for(Node n : newnode.neighbors){
-                if(!visited.containsKey(n)){
-                    visited.put(n,new Node(n.val));
-                    queue.offer(n);
-                }
-                neighbors.add(visited.get(n));
-            }
+        for(Node n:node.neighbors){
+            curr.neighbors.add(clone(n,visited));
         }
         return curr;
-        
-        
-        
         
     }
 }
