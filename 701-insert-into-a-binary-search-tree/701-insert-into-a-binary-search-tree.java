c8c57e62c5450ec79a1ab1e7b9e1b155eb17790f
divit99 <41451985+divit99@users.noreply.github.com>
2022-07-12 01:08:35 +0530

    Time: 0 ms (100.00%), Space: 53.9 MB (66.89%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.java
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.java
@@ -14,25 +14,25 @@
  * }
  */
 class Solution {
-//     public TreeNode insertIntoBST(TreeNode root, int val) {
-        
-//         if(root==null)return new TreeNode(val);
-        
-        
-//         if(root.val<val)root.left=insertIntoBST(root.left,val);
-//         if(root.val>val)root.right=insertIntoBST(root.right,val);
-//         return root;
-        
-//     }
     public TreeNode insertIntoBST(TreeNode root, int val) {
-if (root == null) {
-return new TreeNode(val);
-}
-if (root.val > val) {
-root.left = insertIntoBST(root.left, val);
-} else {
-root.right = insertIntoBST(root.right, val);
-}
-return root;
-}
+        
+        if(root==null)return new TreeNode(val);
+        
+        
+        if(root.val>val)root.left=insertIntoBST(root.left,val);
+        else root.right=insertIntoBST(root.right,val);
+        return root;
+        
+    }
+//     public TreeNode insertIntoBST(TreeNode root, int val) {
+// if (root == null) {
+// return new TreeNode(val);
+// }
+// if (root.val > val) {
+// root.left = insertIntoBST(root.left, val);
+// } else {
+// root.right = insertIntoBST(root.right, val);
+// }
+// return root;
+// }
 }
