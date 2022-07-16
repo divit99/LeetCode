d7c0bebcc74c3c997fec5730b779321796a1d1ba
divit99 <41451985+divit99@users.noreply.github.com>
2022-07-16 16:55:36 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (93.58%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.java
+++ 35-search-insert-position/35-search-insert-position.java
@@ -9,13 +9,8 @@ class Solution {
             if(nums[mid]==target){
                 return mid;
             }
-            
-            if(nums[mid]>target){
-                end=mid-1;
-            }
-            
-            else
-                start=mid+1;
+          if(nums[mid]>target)end=mid-1;
+          else start=mid+1;    
         } 
         return start;
     }
