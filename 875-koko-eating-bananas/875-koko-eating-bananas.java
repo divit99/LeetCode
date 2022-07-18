beed226dbc321b777ed26834581960b93e6b4afe
divit99 <41451985+divit99@users.noreply.github.com>
2022-07-18 23:51:07 +0530

    Time: 68 ms (5.97%), Space: 54.9 MB (6.33%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.java
+++ 875-koko-eating-bananas/875-koko-eating-bananas.java
@@ -1,42 +1,36 @@
 class Solution {
+    
+    public boolean ans(int mid,int arr[],int h){
+        
+         int temp=0;
+            for(int j=0;j<arr.length;j++){
+                int curr=arr[j]/mid;
+                if(arr[j]%mid>0)curr++;
+                temp+=curr;
+            }
+            System.out.println(temp+" "+mid);
+            
+        
+        return temp<=h&&temp>0;
+        
+    }
     public int minEatingSpeed(int[] piles, int h) {
         
-        int max=0;
-        for(int p:piles){
-            max=Math.max(max,p);
-        }
-        
-        if(h==piles.length){
-            return max;
-        }
-        
-        int lo=1;
-        int hi=max;
-        int res=max;
-        
-        while(lo<=hi){
-            int mid=lo+(hi-lo)/2;
-            
-            int time=getHours(piles,mid);
-            
-            
-            if(time<=h){
-                res=Math.min(res,mid);
-                hi=mid-1;
-            }else if(time>h){
-                lo=mid+1;
+        Arrays.sort(piles);
+        int speed=0;
+        int start=1;
+        int end=piles[piles.length-1];
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(ans(mid,piles,h)){
+                speed=mid;
+                end=mid-1;
             }
+         else start=mid+1;
         }
+        return speed;
         
-        return res;
-    }
-    
-    public int getHours(int []piles, int b){
-        int sum=0;
-        for(int i=0;i<piles.length;i++){
-            sum+=Math.ceil((double) piles[i]/b);
-        }
+     
         
-        return sum;
     }
 }
